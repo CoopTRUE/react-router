a28f79b97f565b6d338fbd02111ba2357f58aca9
CoopTRUE <coopdog4545@gmail.com>
2024-11-30 17:27:07 +0200

    added imagetools for image transform

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from './+types/home'
+import Trollface from '~/assets/trollface.png?w=200&format=webp'
 import { Button } from '~/components/ui/button'
 
 export function meta({}: Route.MetaArgs) {
@@ -11,6 +12,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <main className="container mx-auto rounded-lg border bg-card p-4">
+      <img src={Trollface} alt="Trollface" className="mx-auto w-52" />
       <h1>Home</h1>
       <Button onClick={() => alert('clicked')}>Click me</Button>
     </main>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from '@react-router/dev/vite'
 import autoprefixer from 'autoprefixer'
 import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
+import { imagetools } from 'vite-imagetools'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), imagetools()],
 })
